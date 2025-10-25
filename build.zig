d10a7822676184a233a5a32303adb96bf21ddd4f
sirasaki-konoha <rock-db@outlook.jp>
2025-10-25 23:32:48 +0900

    fmt build.zig and remove minisign.zig

--- build.zig
+++ build.zig
@@ -13,7 +13,7 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
-    
+
     // for zig-toml
     const dep_toml = b.dependency("toml", .{
         .target = target,
@@ -36,110 +36,72 @@ pub fn build(b: *std.Build) void {
         .target = target,
     });
 
-    const repos_conf = b.addModule("repos_conf", .{
-        .root_source_file = b.path("src/repos_conf.zig"),
-        .target = target,
-        .imports = &.{
-            .{ .name = "constants", .module = constants },
-        }
-    });
+    const repos_conf = b.addModule("repos_conf", .{ .root_source_file = b.path("src/repos_conf.zig"), .target = target, .imports = &.{
+        .{ .name = "constants", .module = constants },
+    } });
     repos_conf.addImport("toml", dep_toml.module("toml"));
 
-    const fetch = b.addModule("fetch", .{
-        .root_source_file = b.path("src/net/fetch.zig"),
-        .target = target,
-        .imports = &.{
-            .{ .name = "constants", .module = constants },
-        }
-    });
+    const fetch = b.addModule("fetch", .{ .root_source_file = b.path("src/net/fetch.zig"), .target = target, .imports = &.{
+        .{ .name = "constants", .module = constants },
+    } });
     fetch.addImport("curl", dep_curl.module("curl"));
-
 
     const package = b.addModule("package", .{
         .root_source_file = b.path("src/package/package.zig"),
         .target = target,
     });
-    
-    const writer = b.addModule("writer", .{
-        .root_source_file = b.path("src/package/writer/writer.zig"),
-        .target = target,
-        .imports = &.{
-            .{ .name = "package", .module = package },
-        }
-    });
 
-    const reader = b.addModule("reader", .{
-        .root_source_file = b.path("src/package/reader/reader.zig"),
-        .target = target,
-        .imports = &.{
-            .{ .name = "package", .module = package },
-        }
-    });
+    const writer = b.addModule("writer", .{ .root_source_file = b.path("src/package/writer/writer.zig"), .target = target, .imports = &.{
+        .{ .name = "package", .module = package },
+    } });
 
-    const update = b.addModule("update", .{
-        .root_source_file = b.path("src/update/update.zig"),
-        .target = target,
-        .link_libc = true,
-        .imports = &.{
-            .{ .name = "info", .module = info },
-            .{ .name = "fetch", .module = fetch },
-            .{ .name = "constants", .module = constants },
-            .{ .name = "repos_conf", .module = repos_conf },
-            .{ .name = "hash", .module = hash },
-        }
-    });
+    const reader = b.addModule("reader", .{ .root_source_file = b.path("src/package/reader/reader.zig"), .target = target, .imports = &.{
+        .{ .name = "package", .module = package },
+    } });
 
-    const install = b.addModule("install", .{
-        .root_source_file = b.path("src/install/install.zig"),
-        .target = target,
-        .link_libc = true,
-        .imports = &.{
-            .{ .name = "info", .module = info },
-            .{ .name = "fetch", .module = fetch },
-            .{ .name = "constants", .module = constants },
-            .{ .name = "repos_conf", .module = repos_conf },
-            .{ .name = "package_reader", .module = reader },
-            .{ .name = "package", .module = package },
-            .{ .name = "hash", .module = hash },
-        }
-    });
+    const update = b.addModule("update", .{ .root_source_file = b.path("src/update/update.zig"), .target = target, .link_libc = true, .imports = &.{
+        .{ .name = "info", .module = info },
+        .{ .name = "fetch", .module = fetch },
+        .{ .name = "constants", .module = constants },
+        .{ .name = "repos_conf", .module = repos_conf },
+        .{ .name = "hash", .module = hash },
+    } });
 
-    const search = b.addModule("search", .{
-        .root_source_file = b.path("src/search/search.zig"),
-        .target = target,
-        .link_libc = true,
-        .imports = &.{
-            .{ .name = "info", .module = info },
-            .{ .name = "fetch", .module = fetch },
-            .{ .name = "constants", .module = constants },
-            .{ .name = "repos_conf", .module = repos_conf },
-            .{ .name = "package_reader", .module = reader },
-            .{ .name = "package", .module = package },
-        }
-    });
+    const install = b.addModule("install", .{ .root_source_file = b.path("src/install/install.zig"), .target = target, .link_libc = true, .imports = &.{
+        .{ .name = "info", .module = info },
+        .{ .name = "fetch", .module = fetch },
+        .{ .name = "constants", .module = constants },
+        .{ .name = "repos_conf", .module = repos_conf },
+        .{ .name = "package_reader", .module = reader },
+        .{ .name = "package", .module = package },
+        .{ .name = "hash", .module = hash },
+    } });
+
+    const search = b.addModule("search", .{ .root_source_file = b.path("src/search/search.zig"), .target = target, .link_libc = true, .imports = &.{
+        .{ .name = "info", .module = info },
+        .{ .name = "fetch", .module = fetch },
+        .{ .name = "constants", .module = constants },
+        .{ .name = "repos_conf", .module = repos_conf },
+        .{ .name = "package_reader", .module = reader },
+        .{ .name = "package", .module = package },
+    } });
 
     const exe = b.addExecutable(.{
         .name = "hclos",
         .linkage = .static,
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
-            .imports = &.{
-                .{ .name = "fetch", .module = fetch },
-                .{ .name = "update", .module = update },
-                .{ .name = "constants", .module = constants },
-                .{ .name = "info", .module = info },
-                .{ .name = "repos_conf", .module = repos_conf },
-                .{ .name = "reader", .module = reader },
-                .{ .name = "writer", .module = writer },
-                .{ .name = "install", .module = install },
-                .{ .name = "search", .module = search },
-                .{ .name = "hash", .module = hash },
-            }
-        }),
+        .root_module = b.createModule(.{ .root_source_file = b.path("src/main.zig"), .target = target, .optimize = optimize, .imports = &.{
+            .{ .name = "fetch", .module = fetch },
+            .{ .name = "update", .module = update },
+            .{ .name = "constants", .module = constants },
+            .{ .name = "info", .module = info },
+            .{ .name = "repos_conf", .module = repos_conf },
+            .{ .name = "reader", .module = reader },
+            .{ .name = "writer", .module = writer },
+            .{ .name = "install", .module = install },
+            .{ .name = "search", .module = search },
+            .{ .name = "hash", .module = hash },
+        } }),
     });
-
 
     // imports
     exe.root_module.addImport("curl", dep_curl.module("curl"));
--- src/minisign.zig
+++
@@ -1,4 +0,0 @@
-const std = @import("std");
-const constants = @import("constants");
-
-pub fn check_hash(alc: std.mem.Allocator, info: bool, name: []const u8, args: []const []const u8) !bool {}
